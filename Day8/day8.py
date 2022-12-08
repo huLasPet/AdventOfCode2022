055285a03c8b30ff1ec8ff31035b0950b0e0b45b
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-08 18:36:46 +0100

    Day8 Part2

--- Day8/day8.py
+++ Day8/day8.py
@@ -12,7 +12,7 @@ def count_tree(def_row, def_column, def_min_height):
         return def_min_height
 
 
-with open("example.txt") as input_data:
+with open("input.txt") as input_data:
     line = input_data.readlines()
     for item in line:
         trees.append(item.strip())
@@ -20,13 +20,14 @@ with open("example.txt") as input_data:
 height = len(trees)
 width = len(trees[0])
 
+#Create a dict with the coordinates of the trees and their height
 for row_number, row_value in enumerate(trees):
     for column_number, column_value in enumerate(row_value):
         trees_dict[(column_number, row_number)] = int(column_value)
 
 tallest_tree = max(trees_dict.values())
 
-
+#Check the 4 directions
 for row in range(width):
     min_height = -1
     for column in range(height):
@@ -53,7 +54,31 @@ print(f"Part 1 answer: {len(visible_trees)}")
 
 #Part 2
 
-for row in range(width):
-    for column in range(height):
-        print(trees_dict[(row, column)], end=" ")
-    print()
+direction = {"up": (1,0), "down": (-1, 0), "left": (0, -1), "right": (0, 1)}
+best_match = 0
+for row in range(1, width - 1):
+    for column in range(1, height - 1):
+        start_height = trees_dict[(row, column)]
+        scores = []
+        #Grab the values to use when moving the rows and columns
+        for up_down, left_right in direction.values():
+            #Set score for current tree to 0 and set starting columns to use when moving
+            current_score = 0
+            row_move, column_move = row, column
+            while True:
+                #Move to the next row or column up/down or left/right, if not edge or tree too high increase score
+                row_move, column_move = row_move + up_down, column_move + left_right
+                #Reached the edge
+                if row_move < 0 or column_move < 0 or row_move >= width or column_move >= height:
+                    break
+                current_score += 1
+                #Found a tree too high
+                if trees_dict[(row_move, column_move)] >= start_height:
+                    break
+            scores.append(current_score)
+        #Checking and setting a new best_match if needed
+        final_score = scores[0] * scores[1] * scores[2] * scores[3]
+        if final_score > best_match:
+            best_match = final_score
+
+print(f"Part 2 answer: {best_match}")
